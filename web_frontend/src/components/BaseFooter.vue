d5f6a9276c1cee50d223819b75e3a8dc3cc30d88
12345nguyentuananh <kennyanh2426gmail.com>
2023-11-21 17:38:40 +0700

    first commit

---
+++ web_frontend/src/components/BaseFooter.vue
@@ -0,0 +1,187 @@
+<template>
+   <footer id="footer">
+      <div class="first-footer">
+         <div class="container">
+            <div class="d-flex justify-content-between">
+               <div class="contact">
+                  <h5>Thông tin liên hệ</h5>
+                  <ul>
+                     <li class="address">
+                        <i class="fa-sharp fa-solid fa-location-dot icon-contact"></i>
+                        Đại học Cần Thơ
+                     </li>
+                     <li class="phone">
+                        <i class="fa-solid fa-phone icon-phone"></i>
+                        <a style="color: #fff;text-decoration: none;" href="tel:+84 56668888">+84 763901669 </a>
+                     </li>
+                     <li class="email">
+                        <i class="fa-solid fa-envelope icon-email"></i>
+                        <a style="color: #fff;text-decoration: none;" href="mailto:Kennyanh2426@gmail.com">Kennyanh2426@gmail.com</a>
+
+                     </li>
+                  </ul>
+               </div>
+               <div class="column">
+                  <h5>Dịch vụ khách hàng</h5>
+                  <ul>
+                     <li>
+                        <router-link to="/contact">Liên hệ với tôi</router-link>
+                     </li>
+                     <li>
+                        <router-link to="/contact">Về tôi</router-link>
+                     </li>
+                  </ul>
+               </div>
+               <div class="column">
+                  <h5>Mạng xã hội</h5>
+                  <ul>
+                     <li>
+                        <i class="fa-brands fa-facebook"></i>
+                        <a href="https://www.facebook.com"> Facebook</a>
+                     </li>
+                     <li>
+                        <i class="fa-brands fa-twitter"></i>
+                        <a href="https://www.twitter.com"> Twitter</a>
+                     </li>
+                     <li>
+                        <i class="fa-brands fa-instagram"></i>
+                        <a href="https://www.instagram.com"> Instagram</a>
+                     </li>
+                  </ul>
+               </div>
+               <div class="column">
+                  <h5>Thanh toán</h5>
+                  <ul>
+                     <li>
+                        <a href="">Trực tiếp</a>
+                     </li>
+                     <li>
+                        <a href="">Chuyển khoản</a>
+                     </li>
+                  </ul>
+               </div>
+              
+            </div>
+            <div> 
+               <ur>
+                  <h3>Các tỉnh mà chúng tôi phục vụ !</h3>
+                  <li> Hà Nội </li>
+                  <li> Đà Lạt </li>
+                  <li> Bắc Giang </li>
+                  <li> Thái Nguyên </li>
+                  <li> Huế </li>
+                  <li> Hồ Chí Minh </li>
+                  <li> Vĩnh Long </li>
+                  <li> Cần Thơ </li>
+                  <li> Kiên Giang </li>
+                  <li> Cà Mau </li>
+               </ur>
+               
+              
+            </div>
+            <div>
+               <h1>
+                  RAU QUẢ TƯƠI & MUA SẮM TRỰC TUYẾN
+               </h1>
+            </div>
+            <div>
+                  <h6>
+                     Chất lượng của trái cây và rau quả đồng nghĩa với chất lượng của nó
+               sự tươi mát, trong khi việc mua trái cây và rau quả tươi là một công việc tẻ nhạt
+               nhưng với Fraazo bạn có thể giao nó ngay tại nhà
+               . Fraazo mang đến nhiều loại trái cây tươi trang trại,
+               rau và các sản phẩm từ sữa cho bạn, tất cả chỉ trong một cú nhấp chuột. Chúng tôi
+               hiện đang phục vụ khoảng hơn 30 triệu + khách hàng hài lòng ở Hà nội, Đà lạt, Cần thơ, Cà mau ,Vĩnh long. Fraazo đã thực hiện
+               mua sắm trực tuyến các sản phẩm tươi sống dễ dàng hơn nhiều với sự tiện lợi của nó
+               lựa chọn giao hàng tận nhà. Bạn có thể chọn vị trí giao hàng theo
+               sự lựa chọn của bạn và sắp xếp đặt hàng của bạn cho phù hợp. Hoặc bạn có thể đơn giản
+               chọn tùy chọn Chuyển phát nhanh 90 phút và nhận đơn đặt hàng của bạn
+               giao trong thời gian không quá 90 phút. Trang trại trái cây tươi và
+               rau trên Fraazo có sẵn với giá tốt nhất so với
+               các nhà cung cấp và siêu thị khác. Ngoài ra với rất nhiều ưu đãi hấp dẫn,
+               người ta có thể tiết kiệm tới 20% ngân sách mua hàng tạp hóa hàng tháng của mình. Không cần
+               phải xếp hàng dài chờ đợi ở các siêu thị đông đúc hoặc các quán ăn địa phương
+               để được giá rau tốt nhất. Hãy xả stress và chỉ
+               Fraazo liều lượng tươi mát với sự tiết kiệm thoải mái của bạn
+               trang chủ. Fraazo trực tiếp lấy nguồn trái cây và rau tươi từ một
+               mạng lưới nông dân rộng khắp khắp Việt Nam, do đó mang lại
+               bạn có thể sản phẩm tươi đi thẳng từ trang trại đến bàn ăn trong vòng chưa đầy 12
+               giờ. Với bao bì An toàn & Hợp vệ sinh của chúng tôi và số lần sử dụng tối thiểu
+               xử lý, chúng tôi đảm bảo bạn có được sản phẩm tươi sống chất lượng tốt nhất.
+                  </h6>
+               </div>
+               <div class="center">
+                  © Copyrights 2022. Fraazo-Clone. Đã đăng kí bản quyền.
+               </div>
+         </div>
+      </div>
+   </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#footer {
+   display: block;
+}
+
+.first-footer {
+   background-color: #169008;
+   color: #fff;
+   padding: 20px;
+}
+
+#footer h5 {
+   font-size: 18px;
+   color: #fff;
+   margin: 0 0 15px 0;
+   padding: 0 0 7px 0;
+   font-weight: normal;
+}
+
+#footer .contact ul {
+   list-style: none;
+   padding: 0;
+}
+
+#footer .contact ul li {
+   color: #fff;
+   text-decoration: none;
+   display: block;
+   padding: 3px 0;
+   line-height: 20px;
+   font-size: 14px;
+}
+
+#footer .icon-contact {
+   margin-right: 5px;
+   font-size: 16px;
+}
+
+#footer .contact ul li+li {
+   margin-top: 12px;
+}
+
+#footer .column ul {
+   list-style: none;
+   padding: 0;
+}
+
+#footer .column ul li a {
+   color: #fff;
+   text-decoration: none;
+   line-height: 18px;
+   font-size: 15px;
+
+}
+
+#footer .column  ul li:hover {
+   padding-left: 5px;
+}
+
+.center {
+   text-align: center;
+}
+</style>
